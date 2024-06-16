b389e93fd022d6676a3a7bcd633767f6d81b1d97
kaykongo <pkkongo@st.ug.edu.gh>
2024-06-16 06:49:53 +0000

    feat: Inheritance in c# successfully implemented

--- Inheritance/obj/Debug/net8.0/Inheritance.AssemblyInfo.cs
+++ Inheritance/obj/Debug/net8.0/Inheritance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a61e79758edf044470b4b9899778042877ca91ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
